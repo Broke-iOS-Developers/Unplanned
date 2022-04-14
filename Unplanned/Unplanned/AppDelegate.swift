efece438472b25d95a126dd4665ca1dcfa8fb638
Shoaib Huq <slimsho123@gmail.com>
2022-04-14 00:00:43 -0500

    added Parse dependencies

--- Unplanned/Unplanned/AppDelegate.swift
+++ Unplanned/Unplanned/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "l106UQct8EnEDMi0M94ldAi1OHnFkC9EqULqFYtz"
+            $0.clientKey = "BoHoiUaw6XFedJPNMvDDS59Zy13aI9m3SDybWXCy"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
         return true
     }
 
