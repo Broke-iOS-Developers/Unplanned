fd17f6c0416f6509be381fadd5fb44a2a18c4cc5
Shoaib Huq <slimsho123@gmail.com>
2022-04-15 17:55:42 -0500

    finished adding UI for sign up screen

--- Unplanned/Unplanned/Controller/SignUp/SignUpViewController.swift
+++ Unplanned/Unplanned/Controller/SignUp/SignUpViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    //Buttons
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    //Textfields
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "toNextStep", sender: nil)
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "toEnjoyScreen", sender: nil)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
