e7b893124d9352063467a2fd3954d8c8b592a143
dtzeta259 <catalystoftime259@gmail.com>
2022-04-22 13:28:17 -0500

    Screen Look Finished

---
+++ Unplanned/Unplanned/Controller/History/HistoryCell.swift
@@ -0,0 +1,25 @@
+//
+//  HistoryCell.swift
+//  Unplanned
+//
+//  Created by David Teran on 4/22/22.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Unplanned/Unplanned/Controller/History/HistoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HistoryViewController.swift
+//  Unplanned
+//
+//  Created by David Teran on 4/22/22.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    @IBOutlet weak var HistoryTableView: UITableView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var HistoryCell: HistoryCell!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
