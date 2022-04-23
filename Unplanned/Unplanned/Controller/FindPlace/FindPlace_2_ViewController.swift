ac84abd79ae76b3309e4a5057a75bcfd6e95c81f
Shoaib Huq <slimsho123@gmail.com>
2022-04-23 02:34:04 -0500

    Finished Autolayout for screen 2/added labels to VC

--- Unplanned/Unplanned/Controller/FindPlace/FindPlace_2_ViewController.swift
+++ Unplanned/Unplanned/Controller/FindPlace/FindPlace_2_ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class FindPlace_2_ViewController: UIViewController {
 
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var placeTypeLabel: UILabel!
+    @IBOutlet weak var placeImageView: UIImageView!
+    @IBOutlet weak var favoriteStar: UIImageView!
+    @IBOutlet weak var placeStatusLabel: UILabel!
+    @IBOutlet weak var placeHoursLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    @IBOutlet var starImages: [UIImageView]!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func favoriteStarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func newSearchButtonPressed(_ sender: Any) {
+    }
+    @IBAction func getDirectionButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func callButtonPressed(_ sender: UIButton) {
+    }
+    
 }
