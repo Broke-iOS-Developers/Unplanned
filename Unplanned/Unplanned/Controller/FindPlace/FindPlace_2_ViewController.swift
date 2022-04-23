131619448e5e1de9f457ad51df1c6087102f1b60
Shoaib Huq <slimsho123@gmail.com>
2022-04-23 01:53:22 -0500

    Screen 2 UI finished w/o autolayout

---
+++ Unplanned/Unplanned/Controller/FindPlace/FindPlace_2_ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FindPlace_2_ViewController.swift
+//  Unplanned
+//
+//  Created by Shoaib Huq on 4/22/22.
+//
+
+import UIKit
+
+class FindPlace_2_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+}
