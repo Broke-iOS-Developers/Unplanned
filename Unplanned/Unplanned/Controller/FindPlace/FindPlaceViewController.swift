a69c75f1c69027a3c39f3e05452f7ce5beb0aa95
Shoaib Huq <slimsho123@gmail.com>
2022-04-23 22:48:46 -0500

    Added menu functionality to all menuButtons

--- Unplanned/Unplanned/Controller/FindPlace/FindPlaceViewController.swift
+++ Unplanned/Unplanned/Controller/FindPlace/FindPlaceViewController.swift
@@ -28,29 +28,91 @@ class FindPlaceViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-   
     @IBOutlet weak var searchButton: UIButton!{
         didSet{
             searchButton.layer.cornerRadius = 20
             searchButton.layer.masksToBounds = true
         }
     }
+    @IBOutlet weak var categoryMenuButton: UIButton!
+    @IBOutlet weak var ratingMenuButton: UIButton!
+    @IBOutlet weak var openMenuButton: UIButton!
+    @IBOutlet weak var priceMenuButton: UIButton!
+    @IBOutlet weak var radiusTextField: UITextField!
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setPopUpMenus()
     }
-    */
+   
+    // MARK: - Menu Buttons
+    
+    func setPopUpMenus(){
+        
+        //Event Handler
+        let optionsClosure = {(action : UIAction) in print(action.title)}
+        
+        //Category Menu Button
+        categoryMenuButton.titleLabel?.lineBreakMode = .byClipping
+        categoryMenuButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        categoryMenuButton.showsMenuAsPrimaryAction = true
+        categoryMenuButton.changesSelectionAsPrimaryAction = true
+        categoryMenuButton.titleLabel?.numberOfLines = 1
+        categoryMenuButton.menu = UIMenu(children : [
+            UIAction(title: "Food", state: .on, handler: optionsClosure),
+            UIAction(title: "Entertainment", handler: optionsClosure),
+            UIAction(title: "Parks", handler: optionsClosure),
+            UIAction(title: "Bars", handler: optionsClosure),
+
+        ])
+        
+        //Rating Menu Button
+        ratingMenuButton.showsMenuAsPrimaryAction = true
+        ratingMenuButton.changesSelectionAsPrimaryAction = true
+        ratingMenuButton.titleLabel?.numberOfLines = 1
+        ratingMenuButton.menu = UIMenu(children : [
+            UIAction(title: "1+", state: .on, handler: optionsClosure),
+            UIAction(title: "2+", handler: optionsClosure),
+            UIAction(title: "3+", handler: optionsClosure),
+            UIAction(title: "4+", handler: optionsClosure),
+            UIAction(title: "5+", handler: optionsClosure),
+
+
+        ])
+        //Open Menu Button
+        openMenuButton.showsMenuAsPrimaryAction = true
+        openMenuButton.changesSelectionAsPrimaryAction = true
+        openMenuButton.titleLabel?.numberOfLines = 1
+        openMenuButton.menu = UIMenu(children : [
+            UIAction(title: "Yes", state: .on, handler: optionsClosure),
+            UIAction(title: "No", handler: optionsClosure)
+        ])
+        //Price Menu Button
+        priceMenuButton.showsMenuAsPrimaryAction = true
+        priceMenuButton.changesSelectionAsPrimaryAction = true
+        priceMenuButton.titleLabel?.numberOfLines = 1
+        priceMenuButton.menu = UIMenu(children : [
+            UIAction(title: "$", state: .on, handler: optionsClosure),
+            UIAction(title: "$$", handler: optionsClosure),
+            UIAction(title: "$$$", handler: optionsClosure)
+
+        ])
+        
+        
+        
+        
+    }
+    
+    // MARK: - Search Button
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
 
 }
